647e46e9e10aeca1f857d6970a46f593a553f80a
michael-gokey-architect <michael.gokey.architect@gmail.com>
2023-01-27 21:19:43 -0500

    its 9pm on Friday. fix it later

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ ng new channels-angular
 
 from within App folder:
 
-ng g m shared-module
+ng g m /shared-module
 
+ng g c /shared-module/layout/footer 
+
+ng g c /shared-module/layout/header
+
+ng g m /home
+
+ng g c /home/home
 
--- channels-angular/src/app/app.module.ts
+++ channels-angular/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-// import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared-module';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from '../app/shared';
+// import { FooterComponent } from './shared-module/layout/footer/footer.component';
+// import { HeaderComponent } from './shared-module/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // FooterComponent,
+    // HeaderComponent
   ],
   imports: [
-    BrowserModule,
-    // AppRoutingModule,
     SharedModule,
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
